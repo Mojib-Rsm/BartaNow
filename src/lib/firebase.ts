9418fea3ed43525a9843276d71b3b643f4412fb9
MoJiB Rsm <mojibrsm8989@gmail.com>
2025-08-28 03:26:37 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "oftern-wla30",
+  "appId": "1:1086095761160:web:0f362de3f4db558e42d6bd",
+  "storageBucket": "oftern-wla30.firebasestorage.app",
+  "apiKey": "AIzaSyBpW-YDoE3gadsgBEb1N9rVuG8z889O6Fc",
+  "authDomain": "oftern-wla30.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1086095761160"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
