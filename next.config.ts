0a4136a7529f5bcdbc0277b0a00fdc7530927ee1
MoJiB Rsm <mojibrsm8989@gmail.com>
2025-09-05 19:25:23 +0000

    site ti emon vabe kaj koro ze zeno zekon site er iamge add korte pari an

--- next.config.ts
+++ next.config.ts
@@ -16,45 +16,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https' ,
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**',
-      },    
-      {
-        protocol: 'https',
-        hostname: 'www.bartanow.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'barta-now.vercel.app',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        port: '',
-        pathname: '/**',
+       {
+        protocol: 'http',
+        hostname: '**',
       }
     ],
   },
